3c32dfe59f19c503598594572aeb668eb2d4174c
SamanEsmaeili3 <samanesmaeili01@gmail.com>
2024-09-02 23:58:28 +0330

    final push

---
+++ PolygonArea.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolygonArea
+{
+    public sealed class PolygonArea
+    {
+        private static readonly PolygonArea _instance = new PolygonArea();
+        private PolygonArea() { }
+        public static PolygonArea Instance
+        {
+            get { return _instance; }
+        }
+        public double CalculatePolygonArea(List<(double x, double y)> vertices)
+        {
+            int n = vertices.Count;
+            if (n < 3) throw new ArgumentException("A polygon vertices must be bigger than 3");
+
+            double area = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double x1 = vertices[i].x;
+                double y1 = vertices[i].y;
+                double x2 = vertices[(i + 1) % n].x;
+                double y2 = vertices[(i + 1) % n].y;
+
+                area += (x1 * y2) - (y1 * x2);
+            }
+
+            return Math.Abs(area) / 2.0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var polygon = new List<(double x, double y)>
+        {
+            (1, 1), (3, 1), (3, 3), (1, 3) // Vertices of a square
+        };
+var area = PolygonArea.PolygonArea.Instance.CalculatePolygonArea(polygon);
+Console.WriteLine("Area is: " + area);
